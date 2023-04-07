02d3a4b3ac64d231a3c2bd470d0b1d71f7319e41
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-04-06 21:43:27 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "./src/routes";
+import { StackRoutes } from "./src/routes/stackRoutes";
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Routes />
+      <StackRoutes />
     </NavigationContainer>
   );
 }
